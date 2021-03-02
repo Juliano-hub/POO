9d689d8b3f85375903932a8a52e9d612680f370a
Juliano-hub <57720819+Juliano-hub@users.noreply.github.com>
2021-03-02 14:57:05 -0300

    Delete HelloWord2.java

--- HelloWord2.java
+++
@@ -1,8 +0,0 @@
-class HelloWord2 {
-    public static void imprimir(){
-        System.out.println("Bem vindo ao java");
-    }
-        public static void main(String args[]){
-            imprimir();
-    }
-}
