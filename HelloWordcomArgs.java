7c1f32fd41a2f2e16137ebc48c7bee1ec79697c6
Juliano-hub <57720819+Juliano-hub@users.noreply.github.com>
2021-03-02 14:56:58 -0300

    Delete HelloWordcomArgs.java

--- HelloWordcomArgs.java
+++
@@ -1,8 +0,0 @@
-class HelloWordcomArgs{
-    public static void main(String[] args){
-        //nao esquece os argumentos
- 
-        System.out.println("Caro aluno:"+ args[0]+"...bem vindo ao java");
-    }
-    
-}
